d6b8d4bbe90f822b4b7876a9c30530fd76afb6d5
NadaElho <nadaelhosary51@gmail.com>
2024-04-18 17:02:57 +0200

    replace cloundinary with imagekit

---
+++ services/imagekit.service.js
@@ -0,0 +1,54 @@
+var ImageKit = require('imagekit')
+
+var imageKit = new ImageKit({
+  publicKey: 'public_cZl0BKqdHCQ71J3pcY356/HCFOc=',
+  privateKey: 'private_D9vxE4yPVEO2zz0sbEQoBKAT51s=',
+  urlEndpoint: 'https://ik.imagekit.io/wmbo3bdhk',
+})
+
+const imagekitUpload= async (req, res, next) => {
+    try {
+        if (!req.file) {
+            return next();
+        }
+
+       await imageKit.upload({
+            file: req.file.buffer,
+            fileName: req.body.image,
+            folder:'uploads'
+        }, function(err, response) {
+            if (err) {
+                return res.status(500).json({
+                    status: "failed",
+                    message: "An error occurred during file upload. Please try again."
+                });
+            }
+            req.fileId=response.fileId;
+            req.body.image = response.url ;
+            next();
+        });
+    } catch (error) {
+        console.log("Error uploading image to imagekit:", error.message);
+        res.status(500).json({
+            status: "failed",
+            message: "An error occurred during file upload. Please try again."
+        });
+    }
+};
+
+const deleteImage=  async (image) => {  
+      imageKit.deleteFile(image, function(err, result) {
+            if (err) {
+                console.log("Error deleting old image:", err.message);
+            } else {
+                console.log("Old image deleted successfully");
+            }
+        });
+        
+   
+}
+
+module.exports ={
+    imagekitUpload,
+    deleteImage
+}
